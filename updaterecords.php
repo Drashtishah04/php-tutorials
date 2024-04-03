295e22e8a4a76e3400ef3dc34f8771f10f1d63a8
Drashtishah04 <141635058+Drashtishah04@users.noreply.github.com>
2024-04-03 12:59:14 +0530

    Add files via upload

---
+++ updaterecords.php
@@ -0,0 +1,23 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$database="drashti";
+$conn=mysqli_connect($servername,$username,$password,$database );
+if(!$conn){
+	die("sorry we failed to connect: ".mysqli_connect_error());
+}
+else{
+	echo "connection was succesfull<br>";
+}
+$sql="SELECT * FROM `user_login` WHERE `password`='12345'";
+$result=mysqli_query($conn,$sql);
+$num=mysqli_num_rows($result);
+echo $num;
+if($num>0){
+	while($row=mysqli_fetch_assoc($result)){
+		echo var_dump($row);
+		echo "<br>";
+	}
+}
+?>
