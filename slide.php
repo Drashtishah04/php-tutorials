4bc4548c21d6c5ca9bf1e8eb98a807b91341ee95
Drashtishah04 <141635058+Drashtishah04@users.noreply.github.com>
2024-06-07 16:38:11 +0530

    Add files via upload

---
+++ slide.php
@@ -0,0 +1,21 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<style>
+	div
+	{
+		padding:5px;
+		text-align:center;
+		background-color:green;
+		border:solid 1px red;
+	}
+</style>
+<script>
+	$(document).ready(function()
+	{
+		$("#div1").click(function()
+		{
+			$("#div2").slideToggle("slow");
+		});
+	});
+</script>
+<div id="div1">click here</div>
+<div id="div2" style="display:none;">hello world!</div>
