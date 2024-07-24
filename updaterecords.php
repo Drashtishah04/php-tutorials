1e679a3fe57d3850ca82bbd273e6fafe34c116c1
Drashtishah04 <141635058+Drashtishah04@users.noreply.github.com>
2024-07-24 13:24:52 +0530

    Add files via upload

--- updaterecords.php
+++ updaterecords.php
@@ -2,7 +2,7 @@
 $servername="localhost";
 $username="root";
 $password="";
-$database="drashti";
+$database="bscit";
 $conn=mysqli_connect($servername,$username,$password,$database );
 if(!$conn){
 	die("sorry we failed to connect: ".mysqli_connect_error());
